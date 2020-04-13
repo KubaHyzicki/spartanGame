0a1c5b6086d3dd032b913096aa3eb931f9154ec1
KubaHyzicki <kuba.hyzicki@gmail.com>
2020-04-13 19:39:44 +0200

    fix sizes+add default chunk map value

--- draw.vhd
+++ draw.vhd
@@ -7,9 +7,10 @@ entity Draw is
     Port ( PIX_X : in  STD_LOGIC_VECTOR (9 downto 0);
            PIX_Y : in  STD_LOGIC_VECTOR (8 downto 0);
 
-           CHUNK : out STD_LOGIC_VECTOR (4 downto 0);
+           --vector of following format { [X1],[X2],[X3],[X4]  ,  [Y1],[Y2],[Y3],[Y4] }
+           CHUNK : out STD_LOGIC_VECTOR (7 downto 0) := "11111111";
 
-           OBJECT_ID : in STD_LOGIC_VECTOR (3 downto 0);
+           OBJECT_ID : in STD_LOGIC_VECTOR (2 downto 0);
 
            RGB : out  STD_LOGIC_VECTOR (2 downto 0));
 end Draw;
@@ -28,9 +29,14 @@ begin
 	begin
 	    constant chunkX_int : integer := integer(floor(real(PIX_X)/real(chunkSize)));
 	    constant chunkY_int : integer := integer(floor(real(PIX_Y)/real(chunkSize)));
-	    constant CHUNK_X : std_logic_vector (1 downto 0) := conv_std_logic_vector(chunkX_int, 2);
-	    constant CHUNK_Y : std_logic_vector (1 downto 0) := conv_std_logic_vector(chunkY_int, 2);
-	    CHUNK <= CHUNK_X + CHUNK_Y;
+	    if (chunkX_int > 9 or chunkY_int > 9) then
+	   	--default value to be ignored(to be mapped by game to background)
+	    	CHUNK = "11111111";
+	    else
+		    constant CHUNK_X : std_logic_vector (3 downto 0) := conv_std_logic_vector(chunkX_int, 4);
+		    constant CHUNK_Y : std_logic_vector (3 downto 0) := conv_std_logic_vector(chunkY_int, 4);
+		    CHUNK <= CHUNK_X + CHUNK_Y;
+	    end if;
 	end process;
 
 	mapToRGB:
